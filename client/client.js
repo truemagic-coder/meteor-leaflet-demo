91b7acca22d8ede9e10537e68ebfeffa24dc64d6
Wlodek Bzyl <matwb@ug.edu.pl>
2014-03-04 11:09:34 +0100

    simplify adding/removing markers logic:
     * use cursor.observeChanges insted of cursor.observe;
       quote from the Meteor documentation:
         observeChanges is significantly more efficient
         if you do not use addedBefore or movedBefore

--- client/client.js
+++ client/client.js
@@ -4,42 +4,28 @@ Markers = new Meteor.Collection('markers')
 Meteor.subscribe('markers')
 
 Template.map.rendered = function() {
-  // create default image path
   L.Icon.Default.imagePath = 'packages/leaflet/images';
 
-  // create a map in the map div, set the view to a given place and zoom
   var map = L.map('map', {
     doubleClickZoom: false
-  }).setView([49.25044, -123.137], 13);
+  }).setView([54.39591, 18.57327], 12);
 
-  L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+  L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
 
   map.on('dblclick', function(event) {
     Markers.insert({latlng: event.latlng});
   });
 
-  // watch the markers collection  
-  var query = Markers.find({});
-  query.observe({
-    // when new marker - then add to map and when on click then remove
-    added: function(mark) {
-      L.marker(mark.latlng).addTo(map)
+  var query = Markers.find();
+  query.observeChanges({
+    added: function(id, fields) {
+      console.log("insert marker at: ", JSON.stringify(fields.latlng));
+      var marker = L.marker(fields.latlng).addTo(map)
         .on('click', function(event) {
-          // remove the Meteor way (by id and not selector)
-          Markers.remove({ _id: mark._id });
+          map.removeLayer(marker);
+          console.log("removed marker: ", id);
+          Markers.remove({_id: id});
         });
-    },
-    // when removing marker - loop through all layers on the map
-    // and remove the matching layer (marker)
-    removed: function(mark) {
-      var layers = map._layers;
-      // ECMA 5+
-      Object.keys(layers).forEach(function(key) {
-        var val = layers[key];
-        if (val._latlng && val._latlng.equals(mark.latlng)) {
-          map.removeLayer(val);
-        }
-      });
-    }
+    } 
   });
 };
