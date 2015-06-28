36c60e41c22f06fc652da7be2a806441bdaa0372
Bevan Hunt <me@bevanhunt.com>
2015-06-28 15:42:11 -0700

    update policy

--- server/policy.js
+++ server/policy.js
@@ -1,4 +1,4 @@
 // setup local CORS for Cordova testing
-BrowserPolicy.content.allowOriginForAll("http://meteor.local");
+BrowserPolicy.content.allowOriginForAll("meteor.local");
 BrowserPolicy.content.allowOriginForAll("*.openstreetmap.org");
 BrowserPolicy.content.allowOriginForAll("*.tile.thunderforest.com");
