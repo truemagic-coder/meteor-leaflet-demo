ca2b95dd7fe4df839e3cd00c307ebfce7151b2dd
Bevan Hunt <me@bevanhunt.com>
2015-08-23 10:24:34 -0700

    finish geojson example
    add npm package
    
    test
    
    finish demo

--- client/client.js
+++ client/client.js
@@ -3,13 +3,8 @@ Meteor.startup(function() {
   $(window).resize(function() {
     $('#map').css('height', window.innerHeight - 82 - 45);
   });
-  $(window).resize(); // trigger resize event 
+  $(window).resize(); // trigger resize event
 });
- 
-// create marker collection
-var Markers = new Meteor.Collection('markers');
-
-Meteor.subscribe('markers');
 
 Template.map.rendered = function() {
   L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
@@ -19,31 +14,13 @@ Template.map.rendered = function() {
   }).setView([49.25044, -123.137], 13);
 
   L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
-
-  map.on('dblclick', function(event) {
-    Markers.insert({latlng: event.latlng});
-  });
-
-  var query = Markers.find();
-  query.observe({
-    added: function (document) {
-      var marker = L.marker(document.latlng).addTo(map)
-        .on('click', function(event) {
-          map.removeLayer(marker);
-          Markers.remove({_id: document._id});
-        });
-    },
-    removed: function (oldDocument) {
-      layers = map._layers;
-      var key, val;
-      for (key in layers) {
-        val = layers[key];
-        if (val._latlng) {
-          if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
-            map.removeLayer(val);
-          }
-        }
+  Meteor.startup(function() {
+    Meteor.call('getGeoJSON', function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        L.geoJson(result).addTo(map);
       }
-    }
+    });
   });
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-// marker collection
-var Markers = new Meteor.Collection('markers');
-Meteor.publish("markers", function () {
-  return Markers.find();
-});
+var GeoJSON = Meteor.npmRequire('geojson');
+
+Meteor.methods({
+   'getGeoJSON': function getGeoJSON() {
+     var points = [
+       { name: 'Location A', category: 'Store', street: 'Market', lat: 49.25044, lng: -123.137 }
+     ];
+     return GeoJSON.parse(points, {Point: ['lat', 'lng']});
+   }
+ });
 
 // Listen to incoming HTTP requests, can only be used on the server
 WebApp.connectHandlers.use(function(req, res, next) {
