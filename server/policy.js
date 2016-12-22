c472570ab1572aaa336641807d4b2714f53bb7bd
Bevan Hunt <bevan@bevanhunt.com>
2016-12-22 15:38:38 -0800

    update to new leaflet version

--- server/policy.js
+++ server/policy.js
@@ -1,4 +1,7 @@
 // setup local CORS for Cordova testing
 BrowserPolicy.content.allowOriginForAll("meteor.local");
 BrowserPolicy.content.allowOriginForAll("*.openstreetmap.org");
-BrowserPolicy.content.allowOriginForAll("*.tile.thunderforest.com");
+BrowserPolicy.content.allowOriginForAll("stamen-tiles-a.a.ssl.fastly.net");
+BrowserPolicy.content.allowOriginForAll("stamen-tiles-b.a.ssl.fastly.net");
+BrowserPolicy.content.allowOriginForAll("stamen-tiles-c.a.ssl.fastly.net");
+BrowserPolicy.content.allowOriginForAll("stamen-tiles-d.a.ssl.fastly.net");
